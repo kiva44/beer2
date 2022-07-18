6f18327c44aaa4846dbcb801c7bc69e372b69f9a
Ivan Kucherenko <kiva44@gmail.com>
2022-07-18 13:22:41 +0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: ['courses-top.ru']
+  },
+  webpack(config, options) {
+    config.module.rules.push({
+      loader: '@svgr/webpack',
+      options: {
+        prettier: false,
+        svgo: true,
+        svgoConfig: {
+          plugins: [{ removeViewBox: false }],
+        },
+        titleProp: true,
+      },
+      test: /\.svg$/,
+    });
+
+    return config;
+  },
+};
---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import { Htag, Button } from '../components';
+
+export default function Home(): JSX.Element {
+
+  return (
+    <>
+
+      <Htag tag='h1'>Рейтинг пабов по версии "Пиводня"</Htag>
+      <Button appearance='primary'>Кнопка</Button>
+      <Button appearance='ghost'>Кнопка2</Button>
+
+    </>
+  );
+}
